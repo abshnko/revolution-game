6677b8e7610944d6ac0e175831d712c431775b6d
Daria Abushenko <2009add@gmail.com>
2022-12-17 21:21:36 +0500

    changed server deploy url

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
-const url = "https://revolution-game.herokuapp.com/questions";
+const url = 'https://revolution-backend.onrender.com/questions'
 
-export const fetchQuestions = () => axios.get(url);
-export const createQuestion = (newQuestion) => axios.post(url, newQuestion);
-export const updateQuestion = (id, updatedQuestion) =>
-  axios.patch(`${url}/${id}`, updatedQuestion);
-export const deleteQuestion = (id) => axios.delete(`${url}/${id}`);
+export const fetchQuestions = () => axios.get(url)
+export const createQuestion = (newQuestion) => axios.post(url, newQuestion)
+export const updateQuestion = (id, updatedQuestion) => axios.patch(`${url}/${id}`, updatedQuestion)
+export const deleteQuestion = (id) => axios.delete(`${url}/${id}`)
